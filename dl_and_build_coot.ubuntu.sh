abf97aedd7e564fc5dd3100a2faca1e6ee64c120
Jakub Smulski <hgonomeg@gmail.com>
2024-04-11 17:50:01 +0200

    Fix broken gfortran (for Ubuntu this time)

--- dl_and_build_coot.ubuntu.sh
+++ dl_and_build_coot.ubuntu.sh
@@ -8,7 +8,7 @@ install_dependencies_with_distro_package_manager() {
   export DEBIAN_FRONTEND=noninteractive  
   apt update
   apt install -y \
-      git wget gcc-10 g++-10 gfortran-10 gettext pkg-config bison flex make automake gperf vim xmlto libtool-bin \
+      git wget gcc-10 g++-10 gfortran-10 gfortran gettext pkg-config bison flex make automake gperf vim xmlto libtool-bin \
       libdbus-1-dev libmount-dev libexpat1-dev libffi-dev libelf-dev libxml2-dev libxml2-utils libreadline-dev \
       libssl-dev libcurl4-openssl-dev libncurses-dev libsqlite3-dev liblzo2-dev libbz2-dev libpng-dev libbrotli-dev \
       libxcb-glx0-dev \
